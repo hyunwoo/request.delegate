bbefd07bf731e86f3a7d3a958234ac0626d17eec
h.hyunwoo@gmail.com <Angles12>
2018-06-21 10:37:39 +0900

    index pug modified

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/', require('./routes/index'));
 app.use('/request', require('./routes/request'));
 
 // catch 404 and forward to error handler
